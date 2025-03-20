c9df71633a7a878fec335348a53e84169a5f3af4
Khrystyna Storozhuk <khrystynastorozhuk72@gmail.com>
2025-03-19 22:19:04 -0400

    Initial commit

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,7 +1,12 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least 3 characters',
+    'string.max': 'Username should have at most 20 characters',
+    'any.required': 'Username is required',
+  }),
   phoneNumber: Joi.string()
     .pattern(/^[0-9+()-]{6,15}$/)
     .required(),
