59b84e5699bdf867fdac42183f970a293ce6d813
Khrystyna Storozhuk <khrystynastorozhuk72@gmail.com>
2025-03-19 21:20:21 -0400

    Initial commit

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,12 +7,22 @@ export const getAllContacts = async ({
   perPage = 10,
   sortOrder = SORT_ORDER.ASC,
   sortBy = 'name',
+  filter = {},
 }) => {
   const skip = (page - 1) * perPage;
 
-  const contactsCount = await ContactsCollection.countDocuments();
+  const contactsQuery = ContactsCollection.find();
 
-  const contacts = await ContactsCollection.find()
+  if (filter.isFavourite) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+  if (filter.contactType) {
+    contactsQuery.where('contactType').equals(filter.contactType);
+  }
+
+  const contactsCount = await contactsQuery.clone().countDocuments();
+
+  const contacts = await contactsQuery
     .skip(skip)
     .limit(perPage)
     .sort({ [sortBy]: sortOrder })
---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,27 @@
+const parseContactType = (contactType) => {
+  const isString = typeof contactType === 'string';
+  if (!isString) return;
+  const isContactType = ['work', 'personal', 'home'].includes(contactType);
+
+  if (isContactType) return contactType;
+};
+
+const parseIsFavourite = (isFavourite) => {
+  const isString = typeof isFavourite === 'string';
+  if (!isString) return;
+  const isIsFavourite = ['true', 'false'].includes(isFavourite);
+
+  if (isIsFavourite) return isFavourite === 'true';
+};
+
+export const parseFilterParams = (query) => {
+  const { type, isFavourite } = query;
+
+  const parsedContactType = parseContactType(type);
+  const parsedIsFavourite = parseIsFavourite(isFavourite);
+
+  return {
+    contactType: parsedContactType,
+    isFavourite: parsedIsFavourite,
+  };
+};
