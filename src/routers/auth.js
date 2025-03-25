45c214fabd45cc4e5435f3dbe8bd24e4ddc49f33
Khrystyna Storozhuk <khrystynastorozhuk72@gmail.com>
2025-03-25 01:12:38 -0400

    Initial commit

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,12 +4,14 @@ import { registerUserSchema } from '../validation/auth.js';
 import {
   logoutUserController,
   registerUserController,
+  loginUserController,
+  refreshUserSessionController,
 } from '../controllers/auth.js';
+
 import { validateBody } from '../middlewares/validateBody.js';
 import { loginUserSchema } from '../validation/auth.js';
-import { loginUserController } from '../controllers/auth.js';
-import { logoutUserController } from '../controllers/auth.js';
-import { refreshUserSessionController } from '../controllers/auth.js';
+
+import {} from '../controllers/auth.js';
 
 const router = Router();
 
